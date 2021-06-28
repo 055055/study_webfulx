66e94384bb81269f73c0356c77b97aeba0188a26
055055 <snb4301@gmail.com>
2021-06-29 01:02:40 +0900

    WebClient bean - request, response log 추가

--- src/main/java/com/study/webfulx/config/WebClientConfig.java
+++ src/main/java/com/study/webfulx/config/WebClientConfig.java
@@ -3,14 +3,18 @@ package com.study.webfulx.config;
 import io.netty.channel.ChannelOption;
 import io.netty.handler.timeout.ReadTimeoutHandler;
 import io.netty.handler.timeout.WriteTimeoutHandler;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 import reactor.netty.http.client.HttpClient;
 
 import java.util.concurrent.TimeUnit;
 
+@Slf4j
 @Component
 public class WebClientConfig {
 
@@ -22,6 +26,29 @@ public class WebClientConfig {
                         conn.addHandlerLast(new ReadTimeoutHandler(1500, TimeUnit.MILLISECONDS))
                                 .addHandlerLast(new WriteTimeoutHandler(1500, TimeUnit.MILLISECONDS)));
 
-        return WebClient.builder().clientConnector(new ReactorClientHttpConnector(httpClient)).build();
+        return WebClient.builder()
+                .filters(functions -> {
+                    functions.add(logRequest());
+                    functions.add(logResponse());
+                })
+                .clientConnector(new ReactorClientHttpConnector(httpClient)).build();
+    }
+
+
+    public ExchangeFilterFunction logRequest() {
+        return (clientRequest, next) -> {
+            log.info("Request: {} {}", clientRequest.method(), clientRequest.url());
+            clientRequest.headers()
+                    .forEach((name, values) -> values.forEach(value -> log.info("{}={}", name, value)));
+
+            return next.exchange(clientRequest);
+        };
+    }
+
+    public ExchangeFilterFunction logResponse() {
+        return ExchangeFilterFunction.ofResponseProcessor(clientResponse -> {
+            log.info("Response: {}", clientResponse.headers().asHttpHeaders().get("property-header"));
+            return Mono.just(clientResponse);
+        });
     }
 }
